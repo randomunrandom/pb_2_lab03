43b01a6389db4aa7fea9bfa847e1a00f092e6f2e
d4n4i <37236863+d4n4i@users.noreply.github.com>
2018-06-01 09:26:31 +0300

    prettifying

--- main.cpp
+++ main.cpp
@@ -14,19 +14,21 @@ int main(){
     int gddriver = DETECT, gmode, errorcode;
     initgraph(&gddriver, &gmode, "");
 
-    Point p(10, 10, 13);
+    Point p(10, 10, 4);
     p.draw();
     Circle c(20, 20, 10, 13);
     c.draw();
-    Line l(40, 40, 10, 10, 13);
+    Line l(40, 40, 10, 10, 9);
     l.draw();
     Square s(60, 60, 10, 10, 15);
     s.draw();
     Romb r(10, 50, 10, 10, -10, 13);
     r.draw();
-    Rect re(50, 10, 10, 10, 10, 13);
+    r.rotate(3.1415/2);
+    r.draw();
+    Rect re(50, 10, 10, 10, 10, 10);
     re.draw();
-    Parall pa(100, 100, 10, 10, 10, -10, 13);
+    Parall pa(100, 100, 10, 10, 10, -10, 7);
     pa.draw();
 
     getch();
